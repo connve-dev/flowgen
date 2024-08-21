8508942e65698e2b0223d25e44c01f491c8e9130
Jakub Dulny <jdulny@connve.com>
2024-08-21 09:37:32 +0200

    Added salesforce pubsub as git submodule

--- components/salesforce/build.rs
+++ components/salesforce/build.rs
@@ -5,6 +5,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(true)
         .build_server(false)
         .out_dir("src")
-        .compile(&["proto/pubsub/pubsub.proto"], &["proto/pubsub"])?;
+        .compile(&["proto/pubsub/pubsub_api.proto"], &["proto/pubsub"])?;
     Ok(())
 }
