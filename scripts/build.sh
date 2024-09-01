abe736f1f36027b9fdb30f42a515bfd67a2180c5
Jakub Dulny <jdulny@connve.com>
2024-09-01 19:22:13 +0200

    Fix for failing cargo fmt

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ set -e -x
 apt update -qq
 apt-get -qq install pkg-config libssl-dev protobuf-compiler
 apt-get install git
+rustup component add rustfmt
 cd components/google/proto && git clone https://github.com/googleapis/googleapis
 cd components/salesforce/proto && git clone https://github.com/forcedotcom/pub-sub-api
 cargo build --verbose
