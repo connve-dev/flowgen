139c7ba6989169ec0fca5167f0cd3a63fcd191d4
Jakub Dulny <jdulny@connve.com>
2025-03-19 14:48:41 +0100

    File publisher

--- crates/file/src/config.rs
+++ crates/file/src/config.rs
@@ -1,7 +1,4 @@
-use flowgen_core::input::Input;
 use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
-use std::collections::HashMap;
 
 #[derive(PartialEq, Default, Clone, Debug, Deserialize, Serialize)]
 pub struct Source {
