a39f5dee51774796d2e1cb5d3965106c6c752627
Jakub Dulny <jdulny@connve.com>
2025-06-04 11:25:05 +0200

    Improved cache code

--- crates/nats/src/cache.rs
+++ crates/nats/src/cache.rs
@@ -1,49 +1,26 @@
-use async_nats::jetstream;
-use chrono::Utc;
-use flowgen_core::{connect::client::Client as FlowgenClientTrait, stream::event::Event};
-use std::{
-    path::{Path, PathBuf},
-    sync::Arc,
-};
-use tokio::sync::{broadcast::Receiver, Mutex};
-use tracing::{error, event, Level};
+use flowgen_core::connect::client::Client as FlowgenClientTrait;
+use std::path::PathBuf;
 
-/// Base subject used for logging successful event processing messages.
-const DEFAULT_MESSAGE_SUBJECT: &str = "deltalake.writer";
-/// Default alias for the target DeltaTable in MERGE operations.
-const DEFAULT_TARGET_ALIAS: &str = "target";
-/// Default alias for the source data (in-memory table) in MERGE operations.
-const DEFAULT_SOURCE_ALIAS: &str = "source";
-
-/// Errors that can occur during the Delta Lake writing process.
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    /// Error occurring when joining Tokio tasks.
+    /// Nats Client Auth error returned.
     #[error(transparent)]
-    TaskJoin(#[from] tokio::task::JoinError),
-    #[error("failed to connect to a NATS Server")]
-    NatsClientAuth(#[source] crate::client::Error),
-    /// Error originating from the Delta Lake client setup or connection (`super::client`).
+    NatsClientAuth(#[from] crate::client::Error),
     #[error(transparent)]
-    Client(#[from] super::client::Error),
+    NatsKVEntry(#[from] async_nats::jetstream::kv::EntryError),
+    #[error(transparent)]
+    NatsKVPut(#[from] async_nats::jetstream::kv::PutError),
+    #[error(transparent)]
+    NatsKVBucketCreate(#[from] async_nats::jetstream::context::CreateKeyValueError),
+    /// An expected buffer value was empty.
+    #[error("no value in provided buffer")]
+    EmptyBuffer(),
+    /// Internal error: The Cache Store reference was unexpectedly missing.
+    #[error("missing required value Cache Store")]
+    MissingCacheStore(),
     /// An expected attribute or configuration value was missing.
-    #[error("missing required event attrubute")]
-    // Note: "attrubute" typo exists in original code
+    #[error("missing required event attribute")]
     MissingRequiredAttribute(String),
-    /// The required `path` configuration for the Delta table was not provided.
-    #[error("missing required config value path")]
-    MissingPath(),
-    /// Internal error: The DeltaTable reference was unexpectedly missing.
-    #[error("missing required value DeltaTable")]
-    MissingDeltaTable(),
-    /// Could not extract a filename from the configured Delta table path.
-    #[error("no filename in provided path")]
-    EmptyFileName(),
-    /// An expected string value was empty (e.g., filename conversion).
-    #[error("no value in provided str")]
-    EmptyStr(),
-    #[error(transparent)]
-    CacheEntry(#[from] async_nats::jetstream::kv::EntryError),
 }
 
 #[derive(Debug, Default)]
@@ -74,25 +51,25 @@ impl flowgen_core::cache::Cache for Cache {
                     ..Default::default()
                 })
                 .await
-                .unwrap(),
+                .map_err(Error::NatsKVBucketCreate)?,
         };
 
         self.store = Some(store);
         Ok(self)
     }
+
     async fn put(&self, key: &str, value: bytes::Bytes) -> Result<(), Self::Error> {
-        if let Some(store) = &self.store {
-            store.put(key, value).await.unwrap();
-        }
+        let store = self.store.as_ref().ok_or(Error::MissingCacheStore())?;
+        store.put(key, value).await.map_err(Error::NatsKVPut)?;
         Ok(())
     }
     async fn get(&self, key: &str) -> Result<bytes::Bytes, Self::Error> {
-        let store = self.store.as_ref().ok_or(Error::MissingDeltaTable())?;
+        let store = self.store.as_ref().ok_or(Error::MissingCacheStore())?;
         let bytes = store
             .get(key)
             .await
-            .map_err(Error::CacheEntry)?
-            .ok_or(Error::MissingDeltaTable())?;
+            .map_err(Error::NatsKVEntry)?
+            .ok_or(Error::EmptyBuffer())?;
         Ok(bytes)
     }
 }
